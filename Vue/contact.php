30d9a97f1e906a4e4b531307cae06c32346283f9
Comoco75 <Yaxmnamdji@gmail.com>
2024-12-09 20:37:05 +0100

    css

--- Vue/contact.php
+++ Vue/contact.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact</title>
-    <link rel="stylesheet" href="../csscontact.css">
+    <link rel="stylesheet" href="../Vue/csscontact.css">
 </head>
 <body>
  
